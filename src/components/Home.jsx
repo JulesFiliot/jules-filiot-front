3eb7228fe9ce997058fe52146598bd1130270745
julesfiliot <jules.filiot@cpe.fr>
2022-11-19 16:02:19 +0100

    minor changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ import Panel from './UI/Panel';
 
 import '../styles/home.scss';
 
-const itImage1 = require('../images/it_minimalist_compressed.jpg');
+// const itImage1 = require('../images/it_minimalist_compressed.jpg');
 
 function Home() {
   const { t, i18n } = useTranslation();
@@ -20,7 +20,7 @@ function Home() {
           <button type="button" onClick={() => i18n.changeLanguage('fr')}>FR</button>
           <button type="button" onClick={() => i18n.changeLanguage('en')}>EN</button>
         </div>
-        <div>
+        <div className="section">
           <SmallTitle text={t('home.resume.experience')} marginBottom={20} left />
           <Panel
             title={['Freelance', 'VO2-Group', 'Uber', 'McDonald\'s']}
@@ -34,7 +34,19 @@ function Home() {
               ['Full stack cooker using React framework for JavaScript and Nest.js framework for Node.js', 'Web app maintenance', 'Development of new features'],
             ]}
           />
-          <img src={itImage1} className="homeImage" alt="computer on desk" />
+        </div>
+        <div className="section">
+          <SmallTitle text={t('home.resume.education')} marginBottom={20} right />
+          <Panel
+            title={['CPE Lyon', 'Institution des chartreux']}
+            subtitle={['Graduate school of digital science', 'Preparation class for CPE Lyon']}
+            startDate={['2019', '2017']}
+            endDate={['2023', '2019']}
+            description={[
+              ['Gang member', 'Crypto lord', 'Fun provider'],
+              ['Serial lover', 'Work zombie'],
+            ]}
+          />
         </div>
 
         <div style={{ marginBottom: '100vh' }} />
