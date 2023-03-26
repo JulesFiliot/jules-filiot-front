d5fc9cf454ab4f7c31dc10dd92f4badc8d01a3b6
JulesFILIOT <dev@julesfiliot.com>
2023-03-26 21:59:46 +0900

    [WIP] project component

--- src/components/UI/ProjectDisplay.jsx
+++ src/components/UI/ProjectDisplay.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
 import LinkComp from './LinkComp';
 import Image from './Image';
@@ -6,22 +7,31 @@ import Image from './Image';
 import '../../styles/UI/projectDisplay.scss';
 
 function ProjectDisplay({
-  title, description, gitLink, otherLinks, imgData, customClass,
+  title, description, gitLink, otherLinks, imgData, customClass, tags,
 }) {
+  const { t } = useTranslation();
+
   return (
     <div className={`project-display-container${customClass ? ` ${customClass}` : ''}`}>
       <div className="content">
         <div className="project-title">{title}</div>
         <div className="separator" />
         <div className="project-description">{description}</div>
+        <div className="project-tags">{tags || ['react', 'scss', 'api', 'python', 'nestjs', 'ci/cd', 'c#'].map((tag) => (<i>{tag}</i>))}</div>
         <div className="project-links">
+          {(gitLink || (otherLinks && otherLinks.length)) && (
+            <>
+              <div className="links-titles">{t('projectDisplay.links')}</div>
+              <div className="separator tiny" />
+            </>
+          )}
           {gitLink && <LinkComp label={gitLink.title} url={gitLink.url} />}
           {otherLinks && otherLinks.map((link) => (
             <LinkComp key={`${link.title}-${link.url}`} label={link.title} url={link.url} />
           ))}
         </div>
       </div>
-      {imgData && <Image alt={imgData.alt} src={imgData.src} />}
+      {imgData && <Image alt={imgData.alt} src={imgData.src} customClass="project-image" />}
     </div>
   );
 }
@@ -42,12 +52,14 @@ ProjectDisplay.propTypes = {
     alt: PropTypes.string,
   }),
   customClass: PropTypes.string,
+  tags: PropTypes.arrayOf(PropTypes.string),
 };
 ProjectDisplay.defaultProps = {
   gitLink: null,
   otherLinks: null,
   imgData: null,
   customClass: null,
+  tags: null,
 };
 
 export default ProjectDisplay;
