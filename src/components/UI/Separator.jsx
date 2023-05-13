113a9fce456694372a82debe2ca4d3b6c301c58d
JulesFILIOT <dev@julesfiliot.com>
2023-05-14 02:44:05 +0900

    [ADD] separator on mobile view

---
+++ src/components/UI/Separator.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import '../../styles/UI/separator.scss';
+
+function Separator() {
+  return (
+    <div className="separator-line" />
+  );
+}
+
+export default Separator;
