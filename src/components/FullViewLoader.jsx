2f8000be04c10fa9a13aa432cb4afda0bf0f0c61
JulesFILIOT <dev@julesfiliot.com>
2023-05-16 17:30:27 +0900

    [FIX] extensive loading time

--- src/components/FullViewLoader.jsx
+++ src/components/FullViewLoader.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchAllData } from '../actions/dataActions';
@@ -8,26 +8,17 @@ import '../styles/fullViewLoader.scss';
 function FullViewLoader({ children }) {
   const dispatch = useDispatch();
   const { data, loading, error } = useSelector((state) => state);
-  const [showLoader, setShowLoader] = useState(true);
 
   useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      setShowLoader(false);
-    }, 2000);
-
     if (!data) {
       dispatch(fetchAllData());
     }
-
-    return () => {
-      clearTimeout(timeoutId);
-    };
   }, [data]);
 
   if (error) {
     return <div className="loader-container"><span>Error :(</span></div>;
   }
-  if (showLoader || loading || !data) {
+  if (loading) {
     return <div className="loader-container"><span>Loading...</span></div>;
   }
 
